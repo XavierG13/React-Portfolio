2eb9b9c337fa7442d5d12b2dd5669da27128443a
Xavier Guzman <guzmanxavi112@gmail.com>
2021-03-24 00:36:32 -0500

    updated app.js with previous repo

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import AboutMe from "./components/AboutMe/AboutMe";
+import ContactMe from "./components/ContactMe/Contact";
+import Portfolio from "./components/Portfolio/Portfolio";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path={["/", "/About-Me"]}>
+            <AboutMe />
+          </Route>
+          <Route exact path={["/Portfolio"]}>
+            <Portfolio />
+          </Route>
+          <Route exact path={["/Contact"]}>
+            <ContactMe />
+          </Route>
+        </Switch>
+      <Footer />
+      </div>
+    </Router>
   );
 }
 
